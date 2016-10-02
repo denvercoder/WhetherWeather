141c8f55e6f618e3bed608711f0c3ed0f3679af1
Tim Myers <timmyers1978@gmail.com>
2016-10-02 02:39:04 -0600

    ui is set write code now

--- WhetherWeather/ViewController.swift
+++ WhetherWeather/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    @IBOutlet weak var cityTextBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        result.text = "Partly cloudy with a chance of this app being AWESOME!"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func submit(_ sender: AnyObject) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
