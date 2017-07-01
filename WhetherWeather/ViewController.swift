7ca3f3413aaa99254c22100d134f2558cfe65741
Tim Myers <realdenvercoder@gmail.com>
2017-07-01 02:37:46 -0600

    add admob and fastlane

--- WhetherWeather/ViewController.swift
+++ WhetherWeather/ViewController.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import GoogleMobileAds
+import AudioToolbox
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, GADBannerViewDelegate {
 
     @IBOutlet weak var result: UILabel!
     @IBOutlet weak var cityTextBox: UITextField!
     
+    var adMobBannerView = GADBannerView()
+    
+    let ADMOB_BANNER_UNIT_ID = "ca-app-pub-8033231278137446/2225482017"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        initAdMobBanner()
         
     }
 
@@ -27,12 +34,78 @@ class ViewController: UIViewController {
 
     @IBAction func submit(_ sender: AnyObject) {
         
-        cityTextBox.text = ""
+        if let url = URL(string: "http://www.weather-forecast.com/locations/" + cityTextBox.text!.replacingOccurrences(of: " ", with: "-") + "/forecasts/latest") {
+            
+            let request = NSMutableURLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                data, response, error in
+                
+                var message = ""
+                
+                if error != nil {
+                    
+                    print(error ?? "Unknown Error")
+                    
+                } else {
+                    
+                    if let unwrappedData = data {
+                        
+                        
+                        
+                        let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        
+                        
+                        var stringSeparator = "Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">"
+                        
+                        if let contentArray = dataString?.components(separatedBy: stringSeparator) {
+                            
+                            if contentArray.count > 1 {
+                                
+                                stringSeparator = "</span>"
+                                
+                                let newContentArray = contentArray[1].components(separatedBy: stringSeparator)
+                                
+                                if newContentArray.count > 1 {
+                                    
+                                    message = newContentArray[0].replacingOccurrences(of: "&deg;", with: "°")
+                                    
+                                    print(message)
+                                    
+                                }
+                                
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+                if message == "" {
+                    
+                    message = "The weather there couldn't be found. Please try again."
+                    
+                }
+                
+                DispatchQueue.main.sync(execute: {
+                    
+                    self.result.text = message
+                    
+                })
+                
+            }
+            
+            task.resume()
+            
+        } else {
+            
+            result.text = "The weather there couldn't be found. Please try again."
+            
+        }
         
-        let summary = "Mostly dry. Warm (max 81°F on Sun afternoon, min 45°F on Mon night). Winds increasing (light winds from the WSW on Sun morning, fresh winds from the SW by Mon afternoon)."
-        
-        result.text = cityTextBox.text! + " 1 – 3 Day Weather Forecast Summary: " + summary
-
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -43,5 +116,55 @@ class ViewController: UIViewController {
         textField.resignFirstResponder()
         return true
     }
+    
+    // MARK: -  ADMOB BANNER
+    func initAdMobBanner() {
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            // iPhone
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 320, height: 50))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 320, height: 50)
+        } else  {
+            // iPad
+            adMobBannerView.adSize =  GADAdSizeFromCGSize(CGSize(width: 468, height: 60))
+            adMobBannerView.frame = CGRect(x: 0, y: view.frame.size.height, width: 468, height: 60)
+        }
+        
+        adMobBannerView.adUnitID = ADMOB_BANNER_UNIT_ID
+        adMobBannerView.rootViewController = self
+        adMobBannerView.delegate = self
+        view.addSubview(adMobBannerView)
+        
+        let request = GADRequest()
+        adMobBannerView.load(request)
+    }
+    
+    
+    // Hide the banner
+    func hideBanner(_ banner: UIView) {
+        UIView.beginAnimations("hideBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = true
+    }
+    
+    // Show the banner
+    func showBanner(_ banner: UIView) {
+        UIView.beginAnimations("showBanner", context: nil)
+        banner.frame = CGRect(x: view.frame.size.width/2 - banner.frame.size.width/2, y: view.frame.size.height - banner.frame.size.height, width: banner.frame.size.width, height: banner.frame.size.height)
+        UIView.commitAnimations()
+        banner.isHidden = false
+    }
+    
+    // AdMob banner available
+    func adViewDidReceiveAd(_ view: GADBannerView) {
+        showBanner(adMobBannerView)
+    }
+    
+    // NO AdMob banner available
+    func adView(_ view: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        hideBanner(adMobBannerView)
+    }
+    
 }
 
